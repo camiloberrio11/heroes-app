aaf857af999434c6d5e9f4918f916f0982a4127b
jberrio <jberrio@transfiriendo.com>
2020-08-09 22:07:00 -0500

    Se agrega docs

---
+++ docs/precache-manifest.048784af4b2cbc13e8961505bede34fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "944fc9920e2dc332c40a7154b2ac7831",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d92792de78626c17eb60",
+    "url": "/static/js/2.38ae0dc4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.38ae0dc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "656dab8a8c4dbe582498",
+    "url": "/static/js/main.c3f76f24.chunk.js"
+  },
+  {
+    "revision": "9c2e66894db0ee27c93c",
+    "url": "/static/js/runtime-main.37e5ca77.js"
+  }
+]);
